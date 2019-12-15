e4766b262eab17fd23920a344586b3fd3f2ad0f7
Øyvind Monsen <yvind.monsen@gmail.com>
2019-12-15 21:39:33 +0100

    Basic login page setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
+
+import Login from './Login.js';
+import Signup from './Signup.js';
+import Dashboard from './Dashboard.js';
+
+import $ from 'jquery';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+
+	  <Switch>
+	      
+              <Route path="/login">
+		  <Login />
+              </Route>
+
+	      <Route path ="/signup">
+		  <Signup />
+	      </Route>
+	      <Route path="/dashboard">
+		  <Dashboard />
+	      </Route>
+	      <Route path="/">
+		  <Redirect to='/dashboard'/>
+	      </Route>
+	      
+	  </Switch>
+
+      </Router>
+
+
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,61 @@
+import React from "react";
+import './Login.css';
+import $ from 'jquery';
+import {getJwt, setJwt} from "./utils.js";
+
+
+export default function Login() {
+    return (
+	<div className="Userform" id="Signup">
+	    <div className="form-container">
+
+		<h1>Login</h1>
+		
+		<form action="#" onSubmit={loginSubmit} id="signupform">
+		    <label htmlFor="email">Email adress</label>
+		    <input type="text" name="email" id="email" placeholder="Enter your email" />
+
+		    <label htmlFor="password">Password</label>
+		    <input type="password" name="password" id="password" placeholder="Enter your password" />
+
+		    <input type="submit" value="Login" className="submit-btn" />
+		</form>
+	    </div>
+	</div>
+    );
+}
+
+
+function loginSubmit(e) {
+
+    e.preventDefault();
+
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    var data = {
+	"email": email,
+	"password": password
+    };
+
+    
+    var signupURL = "http://localhost:8080/auth/login";
+
+
+    
+    $.ajax({
+	type: "POST",
+	url: signupURL,
+	data: data,
+    }).fail((e) => {
+	console.log(e);
+    }).done((response) => {
+
+	var jwt = response["jwt"];
+
+	setJwt(jwt);
+
+	window.location.href = "/dashboard";
+    });
+    
+}
