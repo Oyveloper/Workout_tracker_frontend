d668b65d21a6629b9114e9915238d95b6837692a
Øyvind Monsen <yvind.monsen@gmail.com>
2020-01-29 21:33:22 +0100

    Added detail view for workouts with graph of entries

--- src/js/WorkouCards.js
+++ src/js/WorkouCards.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/WorkoutCard.css";
 
+import Chart from "chart.js";
 
 export function WorkoutOverviewCard(props) {
     const name = props.name;
@@ -21,14 +22,78 @@ export function WorkoutOverviewCard(props) {
 }
 
 
+function lineChartForWorkout(workout, targetID) {
+    var ctx = document.getElementById(targetID).getContext("2d");
+    var labels = []; 
+    var data = [];
+
+    var counter = 0; 
+    workout.entries.forEach((entry) => {
+	data.push(entry.ammount);
+	labels.push(counter);
+	counter += 1;
+
+    });
+
+    var options = {
+	legend: {
+	    display: false
+	},
+	scales: {
+            xAxes: [{
+		gridLines: {
+                    display:false
+		}
+            }],
+            yAxes: [{
+		gridLines: {
+                    display:false
+		}   
+            }]
+	}
+    }
+
+    var chart = new Chart(ctx, {
+	type: 'line',
+	data: {
+	    labels: labels,
+	    datasets: [{
+		label: "Reps",
+		data: data,
+		fill: false,
+		backgroundColor: "#0099ff",
+		borderColor: "#0099ff",
+		
+	    }]
+	},
+	options: options,
+
+    });
+}
+
+
 export function WorkoutDetailCard(props) {
-    const workoutData = props.workoutData; 
+    const workoutData = props.workoutData;
+
+    const workoutGraph = (
+	<div className="workout-graph-container">
+	    <canvas id="workout-graph" height="70px"></canvas>
+	</div>
+    );
+
+
+
+    setTimeout(() => {
+	lineChartForWorkout(workoutData, "workout-graph");
+    }, 200)
+    
     return (
 	<div className="WorkoutDetailCard">
-	    <h2>{workoutData.name}</h2>
+	    <h1>{workoutData.name}</h1>
 	    <div className="max-container">
-		<div className="max-title">Personal Max</div>
-		<div className="max">{workoutData.maxCount}</div>
+		{workoutGraph}
+		<div className="max-title-detail">Personal Max: {workoutData.maxRep}</div>
+
 	    </div>
 	</div>
     );
