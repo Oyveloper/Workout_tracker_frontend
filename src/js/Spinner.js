02ef10a32fdd6ecb5bbd255fe9e3bb47d2806d0b
Øyvind Monsen <yvind.monsen@gmail.com>
2020-07-08 21:02:47 +0200

    Added loading spinner

---
+++ src/js/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/Spinner.css'
+
+export default function Spinner() {
+    return (
+        <div className="spinnerContainer">
+          <div className="spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          
+        </div>
+    )
+}
